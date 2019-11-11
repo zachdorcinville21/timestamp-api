041c90290468372cc0f07b20869de18c35d3c3f0
zachdorcinville21 <zdorcinville@gmail.com>
2019-11-11 11:56:51 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
+// init project
 var express = require('express');
 var app = express();
 
+var date = new Date();
+
+const dateObj = {
+  unix: Date.now(),
+  utc: Date()
+};
+
 var cors = require('cors');
-app.use(cors({optionSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({optionSuccessStatus: 200}));
 
 app.use(express.static('public'));
 
@@ -10,11 +18,26 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get("/api/timestamp/", (req, res) => {
+  res.json(dateObj);
 });
 
+app.get("/api/timestamp/:date_string", (req, res) => {
+  let dateStr = req.params.date_string;
+  let dateInt;
+
+  if (/\d{5,}/.test(dateStr)) {
+    dateInt = parseInt(dateStr);
+    res.json({ unix: dateStr, utc: new Date(dateInt).toUTCString() });
+  }
+
+  let dateObj = new Date(dateStr);
+
+  if (dateObj.toString() == "Invalid Date") {
+    res.json({ error: "Invalid Date" });
+  }
+  res.json({ unix: dateObj.valueOf(), utc: dateObj.toUTCString() });
+});
 
 var listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
